c8ded86c7a1c7face80ef8a9f6ebf54ce2cb8edf
Ian McDonald <ISMcDonald85@gmail.com>
2024-02-10 20:30:04 -0600

    Add and begin styling of app header

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import AppHeader from "./AppHeader";
+
+function App() {
+  return (
+    <div className="app">
+      <AppHeader />
+    </div>
+  );
+}
+
+export default App;
